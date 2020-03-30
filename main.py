2fbf0cc57e0f2d38b15f76af83b41cc395db16ff
ErLee01 <62352270+ErLee01@users.noreply.github.com>
2020-03-30 11:22:30 -0700

    comments

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Game:
         # initialize game window, etc
         pg.init()
         pg.mixer.init()
+        #sets screen to width and height
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
@@ -34,33 +35,38 @@ class Game:
         self.player = Player(self)
         self.all_sprites.add(self.player)
         ground = Platform(0, HEIGHT-40, WIDTH, 40)
+        #variables for platorms, specs
         plat1 = Platform(200, 400, 150, 20)
         plat2 = Platform(150, 300, 150, 20)
+        #adding in the ground, and platforms.
         self.all_sprites.add(ground)
         self.platforms.add(ground)
         self.all_sprites.add(plat1)
         self.platforms.add(plat1)
         self.all_sprites.add(plat2)
         self.platforms.add(plat2)
+        #spawns in a number of platforms from the given range.
         for plat in range(1,5):
             plat = Platform(random.randint(0, WIDTH), random.randint(0, HEIGHT), 200, 20)
             self.all_sprites.add(plat)
             self.platforms.add(plat)
         self.run()
-
+    #fn for the game to auto run
     def run(self):
         # Game Loop
         self.playing = True
+        #while loop, calls self to do things, like draw in the game and update.
         while self.playing:
             self.clock.tick(FPS)
             self.events()
             self.update()
             self.draw()
-
+    #use this later to have the sprite not hit platforms from x 
     def update(self):
-        # Game Loop - Update
+        #Game Loop - Update
         self.all_sprites.update()
         hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        #teleports player above the platform if it hits the platform.
         if hits:
             self.player.pos.y = hits[0].rect.top+1
             self.player.vel.y = 0
@@ -78,15 +84,15 @@ class Game:
         # Game Loop - draw
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
-        # *after* drawing everything, flip the display
+        #*after* drawing everything, flip the display
         pg.display.flip()
 
     def show_start_screen(self):
-        # game splash/start screen
+        #game splash/start screen
         pass
 
     def show_go_screen(self):
-        # game over/continue
+        #game over/continue
         pass
 
 #more global variables, game for showing screens.
